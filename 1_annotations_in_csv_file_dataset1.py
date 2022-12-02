c755916371a4c78a4ec00bf649a9d55fbae9dfa9
FreudenbergMax <S0566289@htw-berlin.de>
2022-12-02 17:06:31 +0100

    Python-Dateien umbenannt

---
+++ 1_annotations_in_csv_file_dataset1.py
@@ -0,0 +1,50 @@
+import os
+from xml.etree import ElementTree
+import csv
+import glob
+
+
+# hier eine CSV File erstellen
+# dann hier die csv file öffnen und auf write setzen
+
+csvfile = open("UTSoftware/data1.csv", 'w', encoding='utf-8')
+# hier wird der writer für die csv file erstellt
+csvfile_writer = csv.writer(csvfile)
+# hier bekommt die csv file seinen Header
+csvfile_writer.writerow(["root", "id", "breed"])
+
+# die root wo die Annotationen stehen werden hier in die Liste fold geschrieben
+fold = []
+for folders in os.listdir("UTSoftware/Dataset_1/Low-Annotations"):
+    fold.append(folders)
+
+# print(fold)
+# erste Schleife nimmt dann von low Annotations einen Ordner
+# liest dann mit glob glob die ganzen Filenames im Ordner die mit xml enden aus und erstellt eine Liste davon
+for f in fold:
+    r = "UTSoftware/Dataset_1/Low-Annotations/" + f + "/*.xml"
+    roots = glob.glob(r)
+    # print(r)
+    # hier wird dann jedes xml geparst und folder, filename und die Rasse ausgelesen und in die CSV geschrieben
+    for root in roots:
+        xml = ElementTree.parse(root)
+        folder = xml.findtext("folder")
+        filename = xml.findtext("filename")
+        nameU = folder.split("-")
+        name = nameU[2].lower()
+        #Verbesserung Hundenamen
+        if name == "basset":
+            name = "basset_hound"
+        if name == "german_short_haired_pointer":
+            name = "german_shorthaired_pointer"
+        if name == "leonberg":
+            name = "leonberger"
+        if name == "scotch_terrier":
+            name = "scottish_terrier"
+        if name == "staffordshire_bull_terrier":
+            name = "staffordshire_bullterrier"
+        if name == "shiba_dog":
+            name = "shiba_inu"
+
+        csv_line = [folder, filename, name]
+        csvfile_writer.writerow(csv_line)
---
+++ 2_annotations_in_csv_file_dataset2.py
@@ -0,0 +1,55 @@
+import os
+from xml.etree import ElementTree
+import csv
+import glob
+
+
+# function that splits string at first number
+def split_string_at_first_number(string):
+    return [s for s in string.split() if s.isdigit()]
+
+
+# hier eine CSV File erstellen
+# dann hier die csv file öffnen und auf write setzen
+
+csvfile = open("UTSoftware/data2.csv", 'w', encoding='utf-8')
+# hier wird der writer für die csv file erstellt
+csvfile_writer = csv.writer(csvfile)
+# hier bekommt die csv file seinen Header
+csvfile_writer.writerow(["root", "id", "breed"])
+
+# die root wo die Annotationen stehen werden hier in die Liste fold geschrieben
+fold = []
+for folders in os.listdir("UTSoftware/Dataset_2/annotations"):
+    fold.append(folders)
+
+# print(fold)
+# erste Schleife nimmt dann von low Annotations einen Ordner
+# liest dann mit glob glob die ganzen Filenames im Ordner die mit xml enden aus und erstellt eine Liste davon
+for f in fold:
+    r = "UTSoftware/Dataset_2/annotations/" + f + "/*.xml"
+    roots = glob.glob(r)
+    # print(r)
+    # hier wird dann jedes xml geparst und folder, filename und die Rasse ausgelesen und in die CSV geschrieben
+    for root in roots:
+        xml = ElementTree.parse(root)
+        # print(xml.findtext("filename"))
+        # print(xml.getroot().find("object").find("name").text)
+
+        # gibt nur alle daten mit mit tag "dog" aus
+        if xml.getroot().find("object").find("name").text == "dog":
+            filename = xml.findtext("filename")
+            nameU = xml.findtext("filename").split('_')
+            nameU.remove(nameU[-1])
+            name = ""
+            print(nameU)
+            for s in nameU:
+                name = name + s + "_"
+            name = name[:-1].lower()
+            folder = "root"
+            # Verbesserung Hundenamen
+            if name == "german_shorthaired":
+                name = "german_shorthaired_pointer"
+
+            csv_line = [folder, filename, name]
+            csvfile_writer.writerow(csv_line)
